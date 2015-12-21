2bf3d00965cf5b63b81f64af50a624531132b0d5
Martin Matusiak <numerodix@gmail.com>
2015-12-21 17:47:12 +0100

    working trivial cache

---
+++ memcache/src/cache.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+
+pub struct Cache {
+    capacity: u32,
+//    key_maxlen: u32,
+//    value_maxlen: u32,
+    storage: HashMap<Vec<u8>, Vec<u8>>,
+}
+
+
+impl Cache {
+    fn new(capacity: u32) -> Cache {
+        Cache {
+            capacity: capacity,
+            storage: HashMap::new(),
+        }
+    }
+
+    fn get(&self, key: Vec<u8>) -> Option<&Vec<u8>> {
+        // check key size
+
+        self.storage.get(&key)
+    }
+
+    fn set(&mut self, key: Vec<u8>, value: Vec<u8>) {
+        // check key & value sizes
+        // check capacity
+
+        self.storage.insert(key, value);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_one_key() {
+        let mut cache = Cache::new(1);
+
+        let key = "name".to_string().into_bytes();
+        let value = "jimmy".to_string().into_bytes();
+
+        cache.set(key.clone(), value.clone());
+        let value_found = cache.get(key).unwrap();
+
+        assert_eq!(&value, value_found);
+    }
+}
--- memcache/src/main.rs
+++ memcache/src/main.rs
@@ -1,3 +1,6 @@
+mod cache;
+
+
 fn main() {
     println!("Hello, world!");
 }
