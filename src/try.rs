7251d7e191f7b4161207b23a620f35cc5f8a33a7
Martin Matusiak <numerodix@gmail.com>
2015-12-27 00:50:23 +0100

    working demo with driver and two transports and correct multiplexing

--- src/try.rs
+++ src/try.rs
@@ -10,8 +10,8 @@ use protocol::cmd::Resp;
 use protocol::cmd::Value;
 
 
-type CmdSender = Sender<Cmd>;
-type CmdReceiver = Receiver<Cmd>;
+type CmdSender = Sender<(u64, Cmd)>;
+type CmdReceiver = Receiver<(u64, Cmd)>;
 
 type RespSender = Sender<Resp>;
 type RespReceiver = Receiver<Resp>;
@@ -19,9 +19,10 @@ type RespReceiver = Receiver<Resp>;
 
 fn transport(id: u64, sender: CmdSender, receiver: RespReceiver) {
     for i in 0..5 {
-        let val = Cmd::Get(Get::new("x"));
+        let key = format!("x{}", id);
+        let val = Cmd::Get(Get { key: key });
         println!("[transport {}] Sending: {:?}", id, val.clone());
-        sender.send(val.clone()).unwrap();
+        sender.send((id, val.clone())).unwrap();
         println!("[transport {}] Sent: {:?}", id, val.clone());
 
         println!("[transport {}] Receiving...", id);
@@ -34,15 +35,29 @@ fn transport(id: u64, sender: CmdSender, receiver: RespReceiver) {
     println!("[transport {}] Stopping", id);
 }
 
-fn driver(sender: RespSender, receiver: CmdReceiver) {
+fn driver(receiver: CmdReceiver, sender1: RespSender, sender2: RespSender) {
     loop {
         println!("[driver]      Receiving...");
-        let val = receiver.recv().unwrap();
-        println!("[driver]      Received: {:?}", val);
+        let (id, cmd) = receiver.recv().unwrap();
+        println!("[driver]      Received: {:?}", cmd);
 
-        let val = Resp::Value(Value::new("x", vec![1, 2, 3]));
+        let mut key = "?".to_string();
+        match cmd {
+            Cmd::Get(get) => { key = get.key; },
+            _ => (),
+        }
+
+        let val = Resp::Value(Value { key: key, data: vec![1, 2, 3] });
         println!("[driver]      Sending: {:?}", val.clone());
-        sender.send(val.clone()).unwrap();
+        match id {
+            1 => {
+                sender1.send(val.clone()).unwrap();
+            },
+            2 => {
+                sender2.send(val.clone()).unwrap();
+            },
+            _ => (),
+        }
         println!("[driver]      Sent: {:?}", val.clone());
     }
 
@@ -51,16 +66,24 @@ fn driver(sender: RespSender, receiver: CmdReceiver) {
 
 pub fn run_it() {
     let (c_tx, c_rx) = mpsc::channel();
-    let (r_tx, r_rx) = mpsc::channel();
+    let c_tx2 = c_tx.clone();
+
+    let (r_tx1, r_rx1) = mpsc::channel();
+    let (r_tx2, r_rx2) = mpsc::channel();
 
     println!("[listen]      Launching driver");
     thread::spawn(move || {
-        driver(r_tx, c_rx);
+        driver(c_rx, r_tx1, r_tx2);
     });
 
     println!("[listen]      Launching transport {}", 1);
     thread::spawn(move || {
-        transport(1, c_tx, r_rx);
+        transport(1, c_tx, r_rx1);
+    });
+
+    println!("[listen]      Launching transport {}", 2);
+    thread::spawn(move || {
+        transport(2, c_tx2, r_rx2);
     });
 
     loop {
