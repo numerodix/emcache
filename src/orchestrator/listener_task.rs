e458ed7d65a12d4eb4dee0de6d269f7fdf213812
Martin Matusiak <numerodix@gmail.com>
2015-12-27 21:08:55 +0100

    simplify

--- src/orchestrator/listener_task.rs
+++ src/orchestrator/listener_task.rs
@@ -4,10 +4,11 @@ use std::thread;
 
 use super::DriverTask;
 use super::TransportTask;
+use super::TransportId;
 
 
 pub struct ListenerTask {
-    cur_transport_id: u64,
+    cur_transport_id: TransportId,
 }
 
 impl ListenerTask {
@@ -15,32 +16,34 @@ impl ListenerTask {
         ListenerTask { cur_transport_id: 0 }
     }
 
-    pub fn run(&mut self) {
-        // Init
-        let (cmd_tx, cmd_rx) = mpsc::channel();
+    fn next_transport_id(&mut self) -> TransportId {
+        let id = self.cur_transport_id.clone();
+        self.cur_transport_id += 1;
+        id
+    }
 
+    pub fn run(&mut self) {
+        // Initialize the driver
+        let (cmd_tx, cmd_rx) = mpsc::channel();
         let driver_task = DriverTask::new(cmd_rx);
+
         thread::spawn(move || {
             driver_task.run();
         });
 
-        // Start up a server
+        // Start up a tcp server
         let tcp_listener = TcpListener::bind("127.0.0.1:11311").unwrap();
 
         for stream in tcp_listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    // allocate transport id
-                    let id = self.cur_transport_id.clone();
-                    self.cur_transport_id += 1;
-
+                    let id = self.next_transport_id();
                     let cmd_tx = cmd_tx.clone();
                     let transport_task = TransportTask::new(id, cmd_tx);
 
                     thread::spawn(move || {
                         transport_task.run(stream);
                     });
-
                 }
                 Err(_) => {
                     println!("Connection failed :(");
