50a5670974f9298c1f993cb9bfc740d93e53f02c
Martin Matusiak <numerodix@gmail.com>
2015-12-27 12:03:33 +0100

    remove demo code

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,8 @@ mod platform;
 mod protocol;
 mod storage;
 mod tcp_transport;
-mod tcp_server;
-
-mod try;
 
 use orchestrator::ListenerTask;
-use try::run_it;
-use tcp_server::serve_forever;
 
 
 fn main() {
