615991875a2402bae1e790c9095aa9c4f9750b66
Martin Matusiak <numerodix@gmail.com>
2015-12-28 09:02:30 +0100

    add comment

--- src/tcp_transport/metrics.rs
+++ src/tcp_transport/metrics.rs
@@ -1,5 +1,7 @@
 #[derive(Debug, Clone)]
 pub struct TransportMetrics {
+    // These numbers are snapshots given that metrics are recorded concurrently
+    // by each transport and transmitted to the protocol at regular intervals.
     pub bytes_read: u64,
     pub bytes_written: u64,
 }
