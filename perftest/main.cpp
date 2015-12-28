a99a2c543299504feec59fd9ba64c1eea0df7612
Martin Matusiak <numerodix@gmail.com>
2015-12-28 20:32:40 +0100

    add perftest to travis

--- perftest/main.cpp
+++ perftest/main.cpp
@@ -8,12 +8,12 @@
 
 
 int main(int argc, char *argv[]) {
-    MemcacheClient cli("127.0.0.1", 11211);
+    MemcacheClient cli("127.0.0.1", 11311);
     cli.printStats();
 
     std::vector<char> val = {'a', 'b', 'c'};
 
-    for (int i=0; i < 100000; i++) {
+    for (int i=0; i < 10000; i++) {
         assert( true == cli.set("x", val) );
         std::vector<char> val2 = cli.get("x");
         assert( cli.equal(val, val2) );
