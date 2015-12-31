18da5da3d98ed977f43cfbd95df3930e0433b0c7
Martin Matusiak <numerodix@gmail.com>
2015-12-31 16:34:28 +0100

    update integ test script

--- integration_tests.sh
+++ integration_tests.sh
@@ -5,21 +5,24 @@ set -e
 # Build the server
 cargo build
 
+# Options we'll use
+PORT=11311
+
 # Launch the server, give it 2sec to start up
-./run_server.sh >/dev/null &
+./run_server.sh --port $PORT >/dev/null &
 sleep 2
 
 # Run the fill test
 echo -e "\n=== FILL TEST ===\n"
-python -m pyperf.tester -p 11311 --fill 5.0
+python -m pyperf.tester -p $PORT --fill 5.0
 
 # Run the stress test
 echo -e "\n=== STRESS TEST ===\n"
-python -m pyperf.tester -p 11311 --stress
+python -m pyperf.tester -p $PORT --stress
 
 # Run the integ tests
 echo -e "\n=== INTEG TEST ===\n"
-python -m pyperf.tester -p 11311
+python -m pyperf.tester -p $PORT
 exit_code=$?
 
 # Kill the server
