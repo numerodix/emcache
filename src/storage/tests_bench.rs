763d5c712d255b737d8b51df0d54336c6f261fd3
Martin Matusiak <numerodix@gmail.com>
2016-01-01 00:50:20 +0100

    remove pointless comment

--- src/storage/tests_bench.rs
+++ src/storage/tests_bench.rs
@@ -10,7 +10,6 @@ fn bench_set_get_key(b: &mut Bencher) {
     let mut cache = Cache::new(1024);
 
     b.iter(|| {
-        // Set a key
         cache.set(key!(1), value!(9));
         cache.get(&key!(1));
     })
