d284cac37befcf617a3bd8be7377fa46d11d23dd
Martin Matusiak <numerodix@gmail.com>
2016-01-02 21:54:49 +0100

    fix up integ test script

--- integration_tests.sh
+++ integration_tests.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# Build the server
-cargo build --release
-
 # Options we'll use
 PORT=11311
 
+# Build the server
+cargo build --release
+
 # Launch the server, give it 2sec to start up
-./run_server.sh --port $PORT >/dev/null &
+cargo run -- --port $PORT >/dev/null &
 sleep 2
 
 # Run the fill test
@@ -26,6 +26,6 @@ python -m pyperf.tester -p $PORT
 exit_code=$?
 
 # Kill the server
-ps axf | grep target/*/memcache | awk '{print $1}' | xargs kill || true
+ps axf | grep 'target\/.*\/memcache' | awk '{print $1}' | xargs kill || true
 
 exit $exit_code
