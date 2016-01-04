99773231114f2a1d22c981bee07c7727818fa624
Martin Matusiak <numerodix@gmail.com>
2016-01-04 02:33:11 +0100

    factor stress test

--- pyemc/test_stress.py
+++ pyemc/test_stress.py
@@ -4,41 +4,37 @@ from pyemc.abstractions.test_api import TestCase
 
 
 class TestStress(TestCase):
-    def test_get_and_set_const_key_value(self):
+    def run_bench(self, func, loops, desc):
         # establish connection
         self.client.get_stats()
 
-        # measure set rate - noreply
         start_time = time.time()
-        setn_num = 200000  # should take about 2secs
-        for _ in range(setn_num):
+
+        for _ in xrange(loops):
+            func()
+
+        end_time = time.time()
+        interval = end_time - start_time
+
+        rate = float(loops) / interval
+
+        self.write("Made %d %s requests in %.2f seconds = %.2f requests/sec" %
+                   (loops, desc, interval, rate))
+
+    def test_set_const_key_noreply(self):
+        def func():
             self.client.set('x', 'abc', noreply=True)
-        end_time = time.time()
-        setn_interval = end_time - start_time
-        setn_rate = float(setn_num) / (setn_interval) * 2
 
-        # measure set rate
-        start_time = time.time()
-        set_num = 50000  # should take about 2secs
-        for _ in range(set_num):
+        self.run_bench(func, 700000, 'constant key set+noreply')
+
+    def test_set_const_key(self):
+        def func():
             self.client.set('x', 'abc')
-        end_time = time.time()
-        set_interval = end_time - start_time
-        set_rate = float(set_num) / (set_interval) * 2
 
-        # measure get rate
-        start_time = time.time()
-        get_num = 50000  # should take about 2secs
-        for _ in range(get_num):
+        self.run_bench(func, 100000, 'constant key set')
+
+    def test_get_const_key(self):
+        def func():
             self.client.get('x')
-        end_time = time.time()
-        get_interval = end_time - start_time
-        get_rate = float(get_num) / (get_interval) * 2
 
-        # display results
-        self.write("Made %d constant key set+noreply requests in %.2f seconds = %.2f requests/sec" %
-                   (setn_num, setn_interval, setn_rate))
-        self.write("Made %d constant key set requests in %.2f seconds = %.2f requests/sec" %
-                   (set_num, set_interval, set_rate))
-        self.write("Made %d constant key get requests in %.2f seconds = %.2f requests/sec" %
-                   (get_num, get_interval, get_rate))
+        self.run_bench(func, 100000, 'constant key get')
