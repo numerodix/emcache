c8e86a215c0da323354b7ff0ba882f6f2bb8a9c4
Martin Matusiak <numerodix@gmail.com>
2016-01-04 18:42:07 +0100

    fix up transport benchmarks so they only time the relevant code path

--- src/tcp_transport/tests_bench.rs
+++ src/tcp_transport/tests_bench.rs
@@ -13,28 +13,40 @@ use super::TcpTransport;
 
 // Reading
 
+use platform::time::sleep_secs;
 #[bench]
 fn bench_transport_read_cmd_get(b: &mut Bencher) {
-    b.iter(|| {
-        let cmd_str = "get x\r\n".to_string();
-        let ts = TestStream::new(cmd_str.into_bytes());
-        let mut transport = TcpTransport::new(ts);
+    // Prepare a stream "long enough" since we can't tell iter() how many
+    // iterations to use
+    let mut input = vec![];
+    for _ in 0..1000000 {
+        let cmd_str = "get variable\r\n".to_string();
+        input.extend(cmd_str.into_bytes());
+    }
 
-        let cmd = transport.read_cmd().unwrap();
-        assert_eq!(cmd, Cmd::Get(Get::one("x")));
+    let ts = TestStream::new(input);
+    let mut transport = TcpTransport::new(ts);
+
+    b.iter(|| {
+        transport.read_cmd().unwrap();
     })
 }
 
 #[bench]
 fn bench_transport_read_cmd_set(b: &mut Bencher) {
-    b.iter(|| {
-        let cmd_str = "set x 0 0 3 \r\nabc\r\n".to_string();
-        let ts = TestStream::new(cmd_str.into_bytes());
-        let mut transport = TcpTransport::new(ts);
+    // Prepare a stream "long enough" since we can't tell iter() how many
+    // iterations to use
+    let mut input = vec![];
+    for _ in 0..1000000 {
+        let cmd_str = "set variable 13 1 10 noreply\r\n0123456789\r\n".to_string();
+        input.extend(cmd_str.into_bytes());
+    }
 
-        let cmd = transport.read_cmd().unwrap();
-        let exp = Set::new(SetInstr::Set, "x", 0, 0, vec![97, 98, 99], false);
-        assert_eq!(cmd, Cmd::Set(exp));
+    let ts = TestStream::new(input);
+    let mut transport = TcpTransport::new(ts);
+
+    b.iter(|| {
+        transport.read_cmd().unwrap();
     })
 }
 
@@ -43,16 +55,12 @@ fn bench_transport_read_cmd_set(b: &mut Bencher) {
 
 #[bench]
 fn bench_transport_write_resp_value(b: &mut Bencher) {
-    b.iter(|| {
-        let ts = TestStream::new(vec![]);
-        let mut transport = TcpTransport::new(ts);
+    let ts = TestStream::new(vec![]);
+    let mut transport = TcpTransport::new(ts);
 
+    b.iter(|| {
         let val = Value::new("x", 15, "abc".to_string().into_bytes());
         let resp = Resp::Values(vec![val]);
         transport.write_resp(&resp).unwrap();
-        let expected = "VALUE x 15 3\r\nabc\r\nEND\r\n"
-                           .to_string()
-                           .into_bytes();
-        assert_eq!(transport.get_stream().outgoing, expected);
     })
 }
