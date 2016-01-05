0331de19a7dcd9c3b0d4c5de2022c075a7360b33
Martin Matusiak <numerodix@gmail.com>
2016-01-05 22:07:25 +0100

    simplify

--- pyemc/test_integration.py
+++ pyemc/test_integration.py
@@ -280,9 +280,8 @@ class TestApi(TestCase):
 
         # verify that it worked
         item = self.client.get(key)
-        val2 = item.value
 
-        assert val == val2
+        assert val == item.value
 
     # Gets
 
@@ -295,7 +294,6 @@ class TestApi(TestCase):
         item = self.client.gets(key)
 
         # set again, cas_unique should have changed
-        val2 = generate_random_data(10)
         self.client.set(key, val)
         item2 = self.client.gets(key)
         assert item.cas_unique != item2.cas_unique
