72dd77030f248e659b8ac34a8103ab7bfa623f0f
Martin Matusiak <numerodix@gmail.com>
2016-01-13 18:00:15 +0100

    add a test for string_to_str

--- src/common/conversions.rs
+++ src/common/conversions.rs
@@ -5,3 +5,15 @@ pub fn string_to_str<'a>(st: &'a String) -> &'a str {
     // Not really unsafe because the String is already valid so..
     unsafe { str::from_utf8_unchecked(st.as_bytes()) }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::string_to_str;
+
+    #[test]
+    fn test_string_to_str() {
+        let st = "abc".to_string();
+        assert_eq!(&st, string_to_str(&st));
+    }
+}
