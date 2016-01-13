7f83145bcff9a36a8aa12c262b93008df0200b78
Martin Matusiak <numerodix@gmail.com>
2016-01-13 22:55:05 +0100

    add tests for as_string and as_number

--- src/tcp_transport/conversions.rs
+++ src/tcp_transport/conversions.rs
@@ -18,3 +18,36 @@ pub fn as_number<N: FromStr>(bytes: Vec<u8>) -> TcpTransportResult<N> {
         Err(_) => Err(TcpTransportError::NumberParseError),
     }
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use tcp_transport::TcpTransportError;
+
+    use super::as_number;
+    use super::as_string;
+
+
+    #[test]
+    fn test_as_string() {
+        // bytestring is utf8
+        let st = as_string(vec![b'a', b'b']).unwrap();
+        assert_eq!(st, "ab".to_string());
+
+        // bytestring is not utf8
+        let err = as_string(vec![b'a', 254, b'b']).unwrap_err();
+        assert_eq!(err, TcpTransportError::Utf8Error);
+    }
+
+    #[test]
+    fn test_as_number() {
+        // bytestring is a number
+        let num = as_number::<u64>(vec![b'1', b'2']).unwrap();
+        assert_eq!(num, 12);
+
+        // bytestring is not a number
+        let err = as_number::<u64>(vec![b' ', b'1', b'2']).unwrap_err();
+        assert_eq!(err, TcpTransportError::NumberParseError);
+    }
+}
